4a62ce32760ba442ba1d4fe7c8fb975f7c05d491
Mahesh Patnala <maheshwarpatn@gmail.com>
2025-04-25 22:58:46 +0530

    working backend

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { SignedIn, SignedOut, UserButton } from "@clerk/clerk-react"; // ✅ Add this
+import { SignedIn, SignedOut, UserButton } from "@clerk/clerk-react";
 
 export default function Navbar() {
   return (
--- client/src/pages/CommunityPage.jsx
+++ client/src/pages/CommunityPage.jsx
@@ -1,27 +1,57 @@
+import { useEffect, useState } from "react";
 import BatchCard from "../components/community/BatchCard";
+import { useAuth } from "@clerk/clerk-react";
+import {toast} from 'react-toastify'
+import axios from "axios";
 
-const batches = [
-  {
-    year: "2024",
-    totalStudents: 450,
-    image:
-      "https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  },
-  {
-    year: "2025",
-    totalStudents: 480,
-    image:
-      "https://images.pexels.com/photos/1438072/pexels-photo-1438072.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  },
-  {
-    year: "2026",
-    totalStudents: 520,
-    image:
-      "https://images.pexels.com/photos/256541/pexels-photo-256541.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  },
-];
+// const batches = [
+//   {
+//     year: "2024",
+//     totalStudents: 450,
+//     image:
+//       "https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+//   },
+//   {
+//     year: "2025",
+//     totalStudents: 480,
+//     image:
+//       "https://images.pexels.com/photos/1438072/pexels-photo-1438072.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+//   },
+//   {
+//     year: "2026",
+//     totalStudents: 520,
+//     image:
+//       "https://images.pexels.com/photos/256541/pexels-photo-256541.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+//   },
+// ];
 
 const CommunityPage = () => {
+
+  const [batches, setBatches] = useState([])
+  const [loading, setLoading] = useState(true);
+
+  const fetchBatches = async () => {
+    try {
+      const { data } = await axios.get('http://localhost:3000/api/user/batches-list');
+      if (data.success) { 
+        setBatches(data.batches) 
+      }
+      else {
+        toast.error(data.message)
+      }
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchBatches();
+  }, []);
+
+
   return (
     <div className="p-10">
       <div className="mb-8 text-center">
@@ -34,11 +64,15 @@ const CommunityPage = () => {
         </p>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {batches.map((batch) => (
-          <BatchCard key={batch.year} batch={batch} />
-        ))}
-      </div>
+      {loading ? (
+        <p className="text-center text-gray-500">Loading batches...</p>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {batches.map((batch) => (
+            <BatchCard key={batch.id} batch={batch} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
